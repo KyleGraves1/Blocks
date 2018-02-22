34b57901b70955218113162e39f2c2280dececc2
KyleGraves1 <36259992+KyleGraves1@users.noreply.github.com>
2018-02-22 12:00:28 +0100

    blocks

--- javascript/Script.js
+++ javascript/Script.js
@@ -20,4 +20,28 @@ var ctx = p1.getContext("2d");
 ctx.fillStyle = "red";
 ctx.fillRect(playerx, playery, 20 , 20);
 
+
+var up= false;
+var down= false;
+var left= false;
+var right= false;
+
+window.addEventListener("keydown", function(e){
+    if (e.keyCode == 38){up = true}
+    if (e.keyCode == 39){right = true}
+    if (e.keyCode == 40){down = true}
+    if (e.keyCode == 37){left = true}
+})
+
+window.addEventListener("keyup", function(e){
+    if (e.keyCode == 38){up = false}
+    if (e.keyCode == 39){right = false}
+    if (e.keyCode == 40){down = false}
+    if (e.keyCode == 37){left = false}
+})
+
+
+
+
 }
+
